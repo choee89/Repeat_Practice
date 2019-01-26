c45a5ccbe602a28ccf14129b91e7a3749541bbe1
남초이 <choee87@naver.com>
2019-01-26 11:08:25 +0900

    가로로 1~5 출력, 세로 줄바꿈까지.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -16,6 +16,16 @@ public class MainDrive {
 //		문제에서는 가로와 세로중? 세로 줄바꿈 담당
 		for (int i=0 ; i < 5 ; i++) {
 			
+//			바쁘게 돌아가는 반복을 안쪽에 중첩.
+//			문제에서는 가로로 숫자를 찍는 역할 담당.
+			for (int j=0 ; j < 5 ; j++) {
+				System.out.print(j+1);
+			}
+			
+//			가로로 1~5를 찍고나면 줄을 바꿈.
+			System.out.println();
+			
+			
 		}
 		
 		
